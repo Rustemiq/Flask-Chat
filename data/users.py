9f746c6dd6cc51e137d225031f7e8bc7dde04954
Rustemiq <rustem.renatovich@gmail.com>
2025-04-22 21:57:46 +0500

    Добавлена БД и таблица для пользователей

---
+++ data/users.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    username = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    birth_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
