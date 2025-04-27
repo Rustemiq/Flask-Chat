dac31b430d676e2993180024c30afcc4585efe7b
Rustemiq <rustem.renatovich@gmail.com>
2025-04-27 14:20:32 +0500

    добавлен обработчик создания чата

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from flask_login import LoginManager, login_user, login_required, logout_user
 from requests import post
 
 from data.user_recources import UsersListResource
+from data.chat_recources import ChatsListResource
 from forms.user_register import RegisterForm
 from forms.user_login import LoginForm
+from forms.chat_creation import ChatCreationForm
 from data.users import User
 from data import db_session
 
@@ -88,7 +90,40 @@ def logout():
     return redirect("/")
 
 
+@app.route('/create_chat', methods=['GET', 'POST'])
+def create_chat():
+    form = ChatCreationForm()
+    if form.validate_on_submit():
+        if form.add_user.data:
+            form.usernames.append_entry()
+        elif form.delete_user.data:
+            if form.usernames.__len__() > 1:
+                form.usernames.pop_entry()
+        elif form.confirm:
+            db_sess = db_session.create_session()
+            not_found_users = []
+            for username in form.usernames:
+                if not db_sess.query(User).filter(User.username == str(username.data['username'])).first():
+                    not_found_users.append(username)
+            if not_found_users:
+                return render_template(
+                    'chat_creation.html',
+                    form=form,
+                    not_found_users=not_found_users
+                )
+            post('http://127.0.0.1:8080/api/chats', json={
+                'name': form.name.data,
+                'members': form.usernames.data
+                })
+            return redirect('/')
+    return render_template(
+        'chat_creation.html',
+        form=form, not_found_users = []
+    )
+
+
 if __name__ == '__main__':
     db_session.global_init('db/flask_chat.db')
     api.add_resource(UsersListResource, '/api/users')
+    api.add_resource(ChatsListResource, '/api/chats')
     app.run(port='8080', host='127.0.0.1')
