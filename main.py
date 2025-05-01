78d86fa2ca01526a6d4ec1771d37745beffaefed
Rustemiq <rustem.renatovich@gmail.com>
2025-05-01 12:18:05 +0500

    errorhandlers вынесены в соответствующий blueprint

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 from api.user_resources import UsersListResource, UsersResource
 from api.chat_resources import ChatsListResource, ChatsResource
-from blueprints import users_blueprint, chats_blueprint, pages_blueprint
+from blueprints import users_blueprint, chats_blueprint, pages_blueprint, errorhandlers_blueprint
 from data.users import User
 from data.models import db_session
 
@@ -22,16 +22,6 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.errorhandler(400)
-def bad_request(_):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
-
-
 if __name__ == '__main__':
     db_session.global_init('db/flask_chat.db')
     api.add_resource(UsersListResource, '/api/users')
@@ -41,4 +31,5 @@ if __name__ == '__main__':
     app.register_blueprint(users_blueprint.blueprint)
     app.register_blueprint(chats_blueprint.blueprint)
     app.register_blueprint(pages_blueprint.blueprint)
+    app.register_blueprint(errorhandlers_blueprint.blueprint)
     app.run(port='8080', host='127.0.0.1')
