a3e9c79d2691bf54c39dfde3b42aa9b852404284
Rustemiq <rustem.renatovich@gmail.com>
2025-05-10 13:54:15 +0500

    добавлено поле для файлов в форму сообщений

--- forms/message_writing.py
+++ forms/message_writing.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from wtforms import SubmitField, TextAreaField, MultipleFileField
 from wtforms.validators import DataRequired
 
 
 class MessageForm(FlaskForm):
     text = TextAreaField(validators=[DataRequired()])
+    files = MultipleFileField('Files')
     submit = SubmitField('Send')
