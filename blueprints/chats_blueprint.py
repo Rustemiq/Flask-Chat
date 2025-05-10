7d697e48ce08ad5278c3ec1ba56148d061e4cb1c
Rustemiq <rustem.renatovich@gmail.com>
2025-05-10 14:21:06 +0500

    добавлено сохранение пользовательских файлов

--- blueprints/chats_blueprint.py
+++ blueprints/chats_blueprint.py
@@ -1,5 +1,8 @@
+import flask_uploads.exceptions
 from flask import Blueprint, render_template, redirect
 from flask_login import current_user
+from flask_uploads import UploadSet
+from transliterate import translit
 
 from data.db_manager import DbManager
 from forms.chat_creation import ChatCreationForm
@@ -43,8 +46,6 @@ def create_chat():
     )
 
 
-
-
 @blueprint.route('/chat/<int:chat_id>', methods=['GET', 'POST'])
 def chat(chat_id):
     manager = DbManager()
@@ -54,4 +55,9 @@ def chat(chat_id):
     form = MessageForm()
     if form.validate_on_submit():
         manager.create_message(chat_id, current_user.id, form.text.data)
+        messages = UploadSet('messages')
+        for file in form.files.data:
+            if file.filename != '':
+                file.filename = translit(file.filename, language_code='ru', reversed=True)
+                filename = messages.save(file)
     return render_template('chat.html', chat=chat, form=form)
--- forms/message_writing.py
+++ forms/message_writing.py
@@ -1,9 +1,14 @@
+from flask_uploads import UploadSet
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileSize
 from wtforms import SubmitField, TextAreaField, MultipleFileField
 from wtforms.validators import DataRequired
 
 
+messages = UploadSet('messages')
 class MessageForm(FlaskForm):
     text = TextAreaField(validators=[DataRequired()])
-    files = MultipleFileField('Files')
+    files = MultipleFileField('Files', validators=[
+        FileAllowed(messages, 'Unsupported file extension')
+    ])
     submit = SubmitField('Send')
